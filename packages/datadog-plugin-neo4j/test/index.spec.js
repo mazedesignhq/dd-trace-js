757bca846a7ed64856e88e87bdcbb441ca7756fb
Maarten Ackermans <4571935+mackermans@users.noreply.github.com>
2022-07-26 16:32:29 +0200

    add neo4j transaction tests

--- packages/datadog-plugin-neo4j/test/index.spec.js
+++ packages/datadog-plugin-neo4j/test/index.spec.js
@@ -33,10 +33,10 @@ describe('Plugin', () => {
           })
 
           await driver.verifyConnectivity()
+          await driver.session().run('MATCH (n) DETACH DELETE n')
         })
 
         afterEach(async () => {
-          await driver.session().run('MATCH (n) DETACH DELETE n')
           await driver.close()
         })
 
@@ -111,6 +111,92 @@ describe('Plugin', () => {
 
             try {
               await session.run('NOT_EXISTS_OPERATION')
+            } catch (err) {
+              error = err
+            }
+
+            await expectedSpanPromise
+          })
+        })
+
+        describe('transaction', () => {
+          let session
+
+          beforeEach(() => {
+            session = driver.session()
+          })
+
+          afterEach(async () => {
+            await session.close()
+          })
+
+          it('should set the correct tags', async () => {
+            const statement = 'MATCH (m:Movie { name: $name }) RETURN m.name'
+
+            const expectedTagsPromise = agent
+              .use(traces => {
+                const span = traces[0][0]
+
+                expect(span).to.have.property('name', 'neo4j.query')
+                expect(span).to.have.property('service', 'test-neo4j')
+                expect(span).to.have.property('resource', statement)
+                expect(span).to.have.property('type', 'cypher')
+                expect(span.meta).to.have.property('span.kind', 'client')
+                expect(span.meta).to.have.property('db.name', 'neo4j')
+                expect(span.meta).to.have.property('db.type', 'neo4j')
+                expect(span.meta).to.have.property('db.user', 'neo4j')
+                expect(span.meta).to.have.property('out.host', 'localhost')
+                expect(span.metrics).to.have.property('out.port', 11011)
+              })
+
+            const transaction = session.beginTransaction()
+            await transaction.run(statement, { name: 'Alice in Wonderland' })
+            await transaction.commit()
+
+            await expectedTagsPromise
+          })
+
+          it('should propagate context', async () => {
+            const expectedSpanPromise = agent
+              .use(traces => {
+                const span = traces[0][0]
+
+                expect(span).to.include({
+                  name: 'test-context',
+                  service: 'test'
+                })
+
+                expect(span.parent_id).to.not.be.null
+              })
+
+            const span = tracer.startSpan('test-context')
+
+            await tracer.scope().activate(span, async () => {
+              const transaction = session.beginTransaction()
+              await transaction.run('MATCH (n) return n LIMIT 1')
+              await transaction.commit()
+              await span.finish()
+            })
+
+            await expectedSpanPromise
+          })
+
+          it('should handle errors', async () => {
+            let error
+
+            const expectedSpanPromise = agent
+              .use(traces => {
+                const span = traces[0][0]
+
+                expect(span.meta).to.have.property('error.type', error.name)
+                expect(span.meta).to.have.property('error.msg', error.message)
+                expect(span.meta).to.have.property('error.stack', error.stack)
+              })
+
+            try {
+              const transaction = session.beginTransaction()
+              await transaction.run('NOT_EXISTS_OPERATION')
+              await transaction.commit()
             } catch (err) {
               error = err
             }
